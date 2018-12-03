055773ab5582fc94454a8b98fb2b3f6e85fa6fba
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-12-03 22:13:41 +0800

    react counter component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 
 class App extends Component {
   state = {number:0}
-  alertUser = () => {alert("now you know how to handle onclick event")}
   add1toNumber = () => {
     this.state.number++;
-    this.setState({number:this.state.number});
-}
-  
-  minus1toNumber = () => {
-    this.state.number--;
-    this.setState({number:this.state.number});
+    this.setState(this.state);
   }
+  minus1toNumber = () => {
+  this.state.number--;
+  this.setState(this.state);
+}
+
 
   render() {
     return (
       <div className="">
-      <button onClick ={this.add1toNumber}>+</button>
-      <button onClick ={this.minus1toNumber}>-</button>     
+      <button onClick = {this.add1toNumber}>+</button>
+      <button onClick = {this.minus1toNumber}>-</button>
       <span>number:{this.state.number}</span>
       </div>
     );
