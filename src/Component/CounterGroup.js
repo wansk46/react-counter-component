60fcf7f8fde4eaba1fc5b1184d0e5b0907a4161d
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-12-04 15:34:50 +0800

    counter

--- src/Component/CounterGroup.js
+++ src/Component/CounterGroup.js
@@ -6,16 +6,32 @@ class CounterGroup extends Component {
     super(prop)
     this.state = {
       counter: new Array(prop.size).fill(0),
-      sum: 0
+      sum: 0,
     }
   }
   updateSum=(delta) => {
     this.setState({sum: this.state.sum+delta})
   }
+
+  onSubmitFunc = (event) => {
+    console.log(this.refs.inputField.value);
+    event.preventDefault();
+    this.setState({
+      counter: new Array(Number(this.refs.inputField.value)).fill(0),
+      sum: 0,
+    })
+  }
+
+
+
   render() {
     return (
       <div>
         {this.state.counter.map(() => <Counter onUpdate={this.updateSum}/>)}
+        <form onSubmit={this.onSubmitFunc}>
+            <input ref="inputField"/>
+            <button type="submit"> Counter </button>
+        </form>
         <span>sum: {this.state.sum}</span>
       </div>
     );
